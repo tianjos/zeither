fb67398f632d1ed8cd30eaf37c55af10cda27c8a
Thiago Brito <tbaguga@yahoo.com.br>
2021-04-12 19:10:28 -0300

    add more tests

--- tests/test_either.py
+++ tests/test_either.py
@@ -9,3 +9,17 @@ def test_version():
 def test_either_map():
     either = Either(1)
     assert either.map(lambda x: x + 1).value == 2
+
+
+def test_either_map_left_side():
+    either = Either(1, False)
+    assert either.map(lambda x: x + 1).value == 1
+
+
+def test_is_either():
+    either = Either(1)
+    assert Either.is_either(either) == True
+
+
+def test_is_not_either():
+    assert Either.is_either(1) == False
